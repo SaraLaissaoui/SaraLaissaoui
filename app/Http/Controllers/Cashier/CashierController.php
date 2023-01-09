461ec35d69d180b59a157505e3afa5eb080cdf53
Reda Nedjari <ned.r@hotmail.fr>
2023-01-09 17:36:22 +0100

    Controller Imports

--- app/Http/Controllers/Cashier/CashierController.php
+++ app/Http/Controllers/Cashier/CashierController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Cashier;
 
 use App\Http\Controllers\Controller;
+use App\Models\Table;
+use App\Models\Category;
+use App\Models\Food;
+use App\Models\Sale;
+use App\Models\SaleDetail;
 use Illuminate\Http\Request;
 
 class CashierController extends Controller
